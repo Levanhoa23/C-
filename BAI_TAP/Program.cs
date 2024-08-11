7e8738133814ccd2b8324a1a367234a105f207fd
Le Van Hoa <levh2322004@gmail.com>
2024-08-11 22:26:01 +0700

    init

---
+++ BAI_TAP/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAI_TAP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            int b = 4;
+            float j = i + b;
+            Console.WriteLine("Gia tri cua j la = " + j );
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ bai_1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String hoTen;
+            float diemVan;
+            float diemToan;
+            Console.WriteLine("Nhap ho ten");
+            hoTen = Console.ReadLine();
+
+            Console.WriteLine("Moi nhap diem van");
+            diemVan = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Moi nhap diem Toan");
+            diemToan = float.Parse(Console.ReadLine());
+            
+
+            Console.WriteLine("Ho va ten {0} diem toan {1} diem van{2} ", hoTen, diemToan, diemVan);
+            Console.ReadLine();
+Console.ReadKey();
+
+
+
+
+        }
+    }
+}
